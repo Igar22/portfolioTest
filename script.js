93da7ec2ba0e993ec3644b281a663a2b214fb0b2
Igar22 <84812836+Igar22@users.noreply.github.com>
2022-03-23 21:23:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -30,3 +30,60 @@ function changeEn() {
     el.innerHTML = i18Obj['en'][key]
   }
 }
+
+const winter = document.querySelector('.album-winter')
+const spring = document.querySelector('.album-spring')
+const summer = document.querySelector('.album-summer')
+const autumn = document.querySelector('.album-autumn')
+
+const portfolioBtn = document.querySelectorAll('.portfolio-btn')
+
+portfolioBtn[0].style = 'background:#BDAE82;color:#FFFFFF;'
+spring.style = 'display: none;'
+summer.style = 'display: none;'
+autumn.style = 'display: none;'
+
+portfolioBtn[0].addEventListener('click', () => {
+  portfolioBtn[0].style = 'background:#BDAE82;color:#FFFFFF;'
+  portfolioBtn[1].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  portfolioBtn[2].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  portfolioBtn[3].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  winter.style = 'display: block;'
+  spring.style = 'display: none;'
+  summer.style = 'display: none;'
+  autumn.style = 'display: none;'
+})
+
+portfolioBtn[1].addEventListener('click', () => {
+  portfolioBtn[1].style = 'background:#BDAE82;color:#FFFFFF;'
+  portfolioBtn[0].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  portfolioBtn[2].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  portfolioBtn[3].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  winter.style = 'display: none;'
+  spring.style = 'display: block;'
+  summer.style = 'display: none;'
+  autumn.style = 'display: none;'
+})
+
+portfolioBtn[2].addEventListener('click', () => {
+  portfolioBtn[2].style = 'background:#BDAE82;color:#FFFFFF;'
+  portfolioBtn[1].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  portfolioBtn[0].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  portfolioBtn[3].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  winter.style = 'display: none;'
+  spring.style = 'display: none;'
+  summer.style = 'display: block;'
+  autumn.style = 'display: none;'
+})
+
+portfolioBtn[3].addEventListener('click', () => {
+  portfolioBtn[3].style = 'background:#BDAE82;color:#FFFFFF;'
+  portfolioBtn[1].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  portfolioBtn[2].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  portfolioBtn[0].style = 'border-color: #BDAE82;background:#FFFFFF;color:#000000;'
+  winter.style = 'display: none;'
+  spring.style = 'display: none;'
+  summer.style = 'display: none;'
+  autumn.style = 'display: block;'
+})
+
